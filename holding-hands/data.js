bac08dd41a8185c9fab1e98f606bc77ad79b57c4
Maria Luisa <malu.olicosta@gmail.com>
2020-10-02 23:16:10 -0300

    dados do API do projeto holding hands

---
+++ holding-hands/data.js
@@ -0,0 +1,133 @@
+const dados = [
+    {
+        nome: "SOS Mulher",
+        responsavel: "Maria Vasconcellos",
+        categoria: "Mulher e Problemas psicológicos",
+
+        cidade: "Sete Lagoas",
+        uf: "MG",
+
+        descricao: "Atendimento psicológico gratuito para mulheres vítimas de qualquer tipo de violência."
+    },
+
+    {
+        nome: "Escola para todos",
+        responsavel: "Maria Júlia Torres",
+        categoria: "Educação",
+
+        cidade: "Ribeirãozinho",
+        uf: "MT",
+
+        descricao: "Projeto que busca professores voluntários para crianças e adolescentes que não têm condições de irem à escola."
+    },
+
+    {
+        nome: "Tocantins sem fome",
+        responsavel: "Ilza Rodrigues dos Santos",
+        categoria: "Pobreza",
+
+        cidade: "Guaraí",
+        uf: "TO",
+
+        descricao: "Corrente de voluntários para distribuição de alimentos para moradores de rua por vários municípios de Tocantins."
+    },
+    
+    {
+        nome: "Amigos do Meio Ambiente",
+        responsavel: "Iraci de Barros",
+        categoria: "Sustentabilidade",
+
+        cidade: "Manaus",
+        uf: "AM",
+
+        descricao: "Promover práticas de sustentabilidade em escolas com conscientização e adoção de hábitos saudáveis e sustentáveis."
+    },
+
+    {
+        nome: "Ajude os gatinhos",
+        responsavel: "Luísa Costa",
+        categoria: "Causa animal",
+
+        cidade: "Paulista",
+        uf: "PE",
+
+        descricao: "Abrigo de gatos de rua que foram resgatados e precisam de cuidados e adoção."
+    },
+
+    {
+        nome: "Code na Periferia",
+        responsavel: "Rodrigo Alencar",
+        categoria: "Educação",
+
+        cidade: "Rio de Janeiro",
+        uf: "RJ",
+
+        descricao: "Levando o mundo da programação para dentro das comunidades."
+    },
+
+    {
+        nome: "Projeto Inclusão Azul",
+        responsavel: "Emília Bianchi",
+        categoria: "Espectro autismo",
+
+        cidade: "São Paulo",
+        uf: "SP",
+
+        descricao: "Inclusão de crianças autistas nas escolas."
+    },
+
+    {
+        nome: "Projeto Praia Viva",
+        responsavel: "Ana Carolina Cavalcanti",
+        categoria: "Meio ambiente",
+
+        cidade: "Recife",
+        uf: "PE",
+
+        descricao: "Projeto voluntário de retirada de lixo das praias da orla do Recife, Olinda, Paulista e Itamaracá."
+    },
+
+    {
+        nome: "Dia do Noel",
+        responsavel: "Amanda dos Santos",
+        categoria: "Crianças",
+
+        cidade: "Sobral",
+        uf: "CE",
+
+        descricao: "Festa de natal para crianças carentes."
+    },
+
+    {
+        nome: "Projeto Espote Adaptado",
+        responsavel: "Sérgio Evandro de Melo",
+        categoria: "Deficiência física",
+
+        cidade: "Vila Velha",
+        uf: "ES",
+
+        descricao: "Escola de esportes para deficientes físicos."
+    },
+    
+    {
+        nome: "Vote LGBT",
+        responsavel: "Gabriel Motanazzi",
+        categoria: "Causas LGBTQI+",
+
+        cidade: "São Paulo",
+        uf: "SP",
+
+        descricao: "Grupo voluntários que realiza campanhas durante épocas de eleição para divulgar informações sobre perfis de candidatos com propostas pró-LGBT em seus projetos de governo."
+    },
+
+    {
+        nome: "Projeto Amor é acolher",
+        responsavel: "Rose Maria de Alencar",
+
+        cidade: "Itapema",
+        uf: "SC",
+
+        descricao: "Abrigo que acolhe idosos que não possuem família e condições financeiras."
+    },
+
+]
